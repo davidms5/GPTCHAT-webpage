eb5962a9902945bb7f5a692e1f14ff541f22dcf8
david medina <rafaelsanchez97.dm@gmail.com>
2023-02-09 16:12:13 -0300

    header y brand hechos, ahora seccion whatgpt3

--- src/containers/whatGPT3/WhatGPT3.jsx
+++ src/containers/whatGPT3/WhatGPT3.jsx
@@ -1,9 +1,15 @@
 import React from "react";
 import './whatgpt3.css'
-
+import { Feature } from '../../components'
 const WhatGPT3 =() =>{
     return(
-        <div>
+        <div className='gpt3__whatgpt3 section__margin' id="whpt3">
+          <div className='gpt3__whatgpt3-feature'>
+            <Feature />
+      </div>
+      <div>
+
+      </div>
             cosas
         </div>
     )
